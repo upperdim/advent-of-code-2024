366da1562195f40609dba12abd89bfbe1f630d4b
upperdim <45048352+upperdim@users.noreply.github.com>
2024-12-03 07:59:43 +0100

    day 3 solutions

--- day03/day3.py
+++ day03/day3.py
@@ -1,24 +1,52 @@
 # Advent of Code 2024 - Day 3
 
 
+import re
+
+
 def parse_input():
+	line = ''
 	with open('day3_input.txt', encoding="utf8") as f:
-		lines = [line.strip() for line in f.readlines()]
-	return lines
+		for curr_line in f:
+			line += curr_line
+	return line
 
 
-def part1():
-	pass
+def part1(line):
+	results_sum = 0
+	muls = [match.start() for match in re.finditer(r"mul\([0-9]{1,3},[0-9]{1,3}\)", line)]
+	for i in muls:
+		op1 = int(line[line.find('(', i) + 1 : line.find(',', i)])
+		op2 = int(line[line.find(',', i) + 1 : line.find(')', i)])
+		results_sum += op1 * op2
+	print(f'{results_sum}')
 
 
-def part2():
-	pass
+def part2(line):
+	results_sum = 0
+	dos = [match.start() for match in re.finditer(r"do\(\)", line)]
+	donts = [match.start() for match in re.finditer(r"don't\(\)", line)]
+	muls = [match.start() for match in re.finditer(r"mul\([0-9]{1,3},[0-9]{1,3}\)", line)]
+	for i in muls:
+		do_operation = True
+		# Find the first instruction before current multiplication. If none, do it.
+		for j in range(i - 1, -1, -1):
+			if j in dos:
+				break
+			if j in donts:
+				do_operation = False
+				break
+		if do_operation:
+			op1 = int(line[line.find('(', i) + 1 : line.find(',', i)])
+			op2 = int(line[line.find(',', i) + 1 : line.find(')', i)])
+			results_sum += op1 * op2
+	print(f'{results_sum}')
 
 
 def main():
-	lines = parse_input()
-	part1()
-	part2()
+	line = parse_input()
+	part1(line)
+	part2(line)
 
 
 if __name__ == '__main__':
--- demo.py
+++ demo.py
@@ -4,3 +4,6 @@ l2 = l1
 print(f'l2 = {l2}')
 l2.pop(1)
 print(f'l1 = {l1}')
+
+for r in range(10, -1, -1):
+	print(r)
