8fdeee386865c63f3ff2e054b2adde33aaef5f2a
upperdim <45048352+upperdim@users.noreply.github.com>
2024-12-04 06:51:50 +0100

    removed unused list

--- day04/day4.py
+++ day04/day4.py
@@ -59,16 +59,6 @@ def part1(grid):
 
 
 def part2(grid):
-	dir_offsets = [
-		[-1, -1],  # up left
-		[-1,  0],  # up
-		[-1,  1],  # up right
-		[ 0,  1],  # right
-		[ 1,  1],  # down right
-		[ 1,  0],  # down
-		[ 1, -1],  # down left
-		[ 0, -1]   # left
-	]
 	x_mas_count = 0
 	for r, row in enumerate(grid):
 		for c, ch in enumerate(row):
