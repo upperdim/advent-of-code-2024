333810ba2332a5df99e9dacdea1768d5c147c1d1
upperdim <45048352+upperdim@users.noreply.github.com>
2024-12-08 05:50:24 +0100

    template for day 8

---
+++ day08/day8.py
@@ -0,0 +1,24 @@
+# Advent of Code 2024 - Day 8
+
+
+def parse_input():
+	with open('day8_test.txt', 'r') as f:
+		lines = [line.strip() for line in f.readlines()]
+	return lines
+
+
+def part1(lines):
+	pass
+
+
+def part2(lines):
+	pass
+
+def main():
+	lines = parse_input()
+	part1(lines)
+	part2(lines)
+
+
+if __name__ == '__main__':
+	main()
