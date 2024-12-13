794d706bee2f78172468b74dd7301ada6bb730c3
upperdim <45048352+upperdim@users.noreply.github.com>
2024-12-13 09:08:51 +0100

    day 13 solution

--- day13/day13.py
+++ day13/day13.py
@@ -2,6 +2,7 @@
 
 
 import pprint
+from math import floor
 
 
 def parse_input():
@@ -31,19 +32,21 @@ def parse_input():
 
 # na * dx_a + nb * dx_b = px
 # na * dy_a + nb * dy_b = py
-# na + nb <= 100
+# na <= 100
+# nb <= 100
 def part1(arr):
 	total_min_toks = 0
-	for i in range(len(arr)):
+	for i in range(len(arr[0])):
+		dx_a, dy_a = arr[0][i][0], arr[0][i][1]
+		dx_b, dy_b = arr[1][i][0], arr[1][i][1]
+		px  , py   = arr[2][i][0], arr[2][i][1]
+		
 		min_tok = None
 		for na in range(100):
-			for nb in range(100):	
-				dx_a, dy_a = arr[0][i][0], arr[0][i][1]
-				dx_b, dy_b = arr[1][i][0], arr[1][i][1]
-				px  , py   = arr[2][i][0], arr[2][i][1]
-
-				if na * dx_a + nb * dx_b == px and na * dy_a + nb * dy_b == py:
-					print(f'FOUND:  {na} * {dx_a} + {nb} * {dx_b} == {px} and {na} * {dy_a} + {nb} * {dy_b} == {py}')
+			for nb in range(100):
+				# print(f'Checking {na}*{dx_a} + {nb}*{dx_b} == {px} and {na}*{dy_a} + {nb}*{dy_b} == {py}')
+				if na*dx_a + nb*dx_b == px and na*dy_a + nb*dy_b == py:
+					# print(f'FOUND:  {na}*{dx_a} + {nb}*{dx_b} == {px} and {na}*{dy_a} + {nb}*{dy_b} == {py}')
 					curr_tok = (3 * na + nb)
 
 					if min_tok is None or curr_tok < min_tok:
@@ -55,7 +58,18 @@ def part1(arr):
 
 
 def part2(arr):
-	pass
+	total_min_toks = 0
+	for i in range(len(arr[0])):
+		dx_a, dy_a = arr[0][i][0], arr[0][i][1]
+		dx_b, dy_b = arr[1][i][0], arr[1][i][1]
+		px  , py   = arr[2][i][0] + 10000000000000, arr[2][i][1] + 10000000000000
+
+		na = (px * dy_b - py * dx_b) / (dx_a * dy_b - dy_a * dx_b)
+		nb = (px - dx_a * na) / dx_b
+		if floor(na) == na and floor(nb) == nb:
+			curr_toks = na * 3 + nb
+			total_min_toks += int(curr_toks)
+	print(total_min_toks)
 
 
 def main():
