83a3112725fdfad48e364f2541834ee88bb4d6b2
upperdim <45048352+upperdim@users.noreply.github.com>
2024-12-13 07:20:13 +0100

    part 1 wip

--- day13/day13.py
+++ day13/day13.py
@@ -1,24 +1,67 @@
 # Advent of Code 2024 - Day 13
 
 
+import pprint
+
+
 def parse_input():
-	with open('day13_test.txt', encoding="utf8") as f:
-		lines = [line.strip() for line in f.readlines()]
-	return lines
+	a = []
+	b = []
+	p = []
+	state = 'a'
+	with open('day13_input.txt', encoding="utf8") as f:
+		for line in f.readlines():
+			if line == '\n':
+				continue
+			dx_start = line.find('X')
+			dx, dy = line[dx_start:].split(', ')
+			dx = int(dx[2:])
+			dy = int(dy[2:])
+			if state == 'a':
+				a.append([dx, dy])
+				state = 'b'
+			elif state == 'b':
+				b.append([dx, dy])
+				state = 'p'
+			elif state == 'p':
+				p.append([dx, dy])
+				state = 'a'
+		return [a, b, p]
 
 
-def part1(lines):
-	pass
+# na * dx_a + nb * dx_b = px
+# na * dy_a + nb * dy_b = py
+# na + nb <= 100
+def part1(arr):
+	total_min_toks = 0
+	for i in range(len(arr)):
+		min_tok = None
+		for na in range(100):
+			for nb in range(100):	
+				dx_a, dy_a = arr[0][i][0], arr[0][i][1]
+				dx_b, dy_b = arr[1][i][0], arr[1][i][1]
+				px  , py   = arr[2][i][0], arr[2][i][1]
+
+				if na * dx_a + nb * dx_b == px and na * dy_a + nb * dy_b == py:
+					print(f'FOUND:  {na} * {dx_a} + {nb} * {dx_b} == {px} and {na} * {dy_a} + {nb} * {dy_b} == {py}')
+					curr_tok = (3 * na + nb)
+
+					if min_tok is None or curr_tok < min_tok:
+						min_tok = curr_tok
+
+		if min_tok is not None:
+			total_min_toks += min_tok
+	print(total_min_toks)
 
 
-def part2(lines):
+def part2(arr):
 	pass
 
 
 def main():
-	lines = parse_input()
-	part1(lines)
-	part2(lines)
+	arr = parse_input()
+	part1(arr)
+	part2(arr)
 
 
 if __name__ == '__main__':
