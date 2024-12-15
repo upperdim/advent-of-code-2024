af9751fc308358c9ff031f252ea21c1701206ee5
upperdim <45048352+upperdim@users.noreply.github.com>
2024-12-15 20:04:17 +0100

    day 16 template

---
+++ day16/day16.py
@@ -0,0 +1,28 @@
+# Advent of Code 2024 - Day 16
+
+
+import pprint
+
+
+def parse_input():
+	with open('day16_test.txt', encoding="utf8") as f:
+		lines = [line.strip() for line in f.readlines()]
+	return lines
+
+
+def part1(lines):
+	pass
+
+
+def part2(lines):
+	pass
+
+
+def main():
+	lines = parse_input()
+	part1(lines)
+	part2(lines)
+
+
+if __name__ == '__main__':
+	main()
