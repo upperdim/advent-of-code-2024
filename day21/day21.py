3bb574783ceecd5b0495324d3e77912bc2c87fa0
upperdim <45048352+upperdim@users.noreply.github.com>
2024-12-21 05:58:38 +0100

    templates between day 18 and 21

---
+++ day21/day21.py
@@ -0,0 +1,22 @@
+def parse_input():
+	with open('day21_input.txt', encoding="utf8") as f:
+		lines = [line.strip() for line in f.readlines()]
+	return lines
+
+
+def part1(lines):
+	pass
+
+
+def part2(lines):
+	pass
+
+
+def main():
+	lines = parse_input()
+	part1(lines)
+	part2(lines)
+
+
+if __name__ == '__main__':
+	main()
