11f0abc832db4500aa19b6211520299f7b877a1b
upperdim <45048352+upperdim@users.noreply.github.com>
2024-12-22 09:14:50 +0100

    day22 template

---
+++ day22/day22.py
@@ -0,0 +1,25 @@
+from pprint import pp
+
+
+def parse_input():
+	with open('day22_test.txt', encoding="utf8") as f:
+		lines = [line.strip() for line in f.readlines()]
+	return lines
+
+
+def part1(lines):
+	pass
+
+
+def part2(lines):
+	pass
+
+
+def main():
+	lines = parse_input()
+	part1(lines)
+	part2(lines)
+
+
+if __name__ == '__main__':
+	main()
