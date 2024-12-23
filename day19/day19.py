6d1a73657b55fced77387a2d5eeda5bad44c8059
upperdim <45048352+upperdim@users.noreply.github.com>
2024-12-23 11:20:51 +0100

    day 19 part 1 solution

--- day19/day19.py
+++ day19/day19.py
@@ -1,21 +1,40 @@
+from pprint import pp
+
+
 def parse_input():
 	with open('day19_input.txt', encoding="utf8") as f:
 		lines = [line.strip() for line in f.readlines()]
-	return lines
+	towels = lines[0].split(', ')
+	patterns = [line for line in lines[2:]]
+	return towels, patterns
 
 
-def part1(lines):
-	pass
+def is_possible(pattern, towels):
+	if pattern == '':
+		return True
+	for towel in towels:
+		if towel == pattern[:len(towel)]:
+			if is_possible(pattern[len(towel):], towels):
+				return True
+	return False
 
 
-def part2(lines):
+def part1(towels, patterns):
+	possible_count = 0
+	for pattern in patterns: 
+		if is_possible(pattern, towels):
+			possible_count += 1
+	print(f'Possible = {possible_count}')
+
+
+def part2(towels, patterns):
 	pass
 
 
 def main():
-	lines = parse_input()
-	part1(lines)
-	part2(lines)
+	towels, patterns = parse_input()
+	part1(towels, patterns)
+	part2(towels, patterns)
 
 
 if __name__ == '__main__':
